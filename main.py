f31f5975033a84da4febafa89e16dc1067954be8
Zhijun Erica Lei <zhijun_0705@126.com>
2017-01-08 11:21:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#Setup
+import sys
+import pygame
+
+FPS = 30
+SCREENWIDTH  = 800
+SCREENHEIGHT = 600
+# amount by which base can maximum shift to left
+GAPSIZE  = 100 # gap between obstacles??
+
+# image, sound and hitmask  dicts
+IMAGES, SOUNDS, HITMASKS = {}, {}, {}
