51519661829620291713b60001e5969560f5369d
arthurbumblebee <amakumbi@colby.edu>
2017-01-15 16:52:52 -0500

    updated stable version with new screen dimensions and constants.py file for uniformity across different classes

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""
+Global constants
+"""
+
+# Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+
+
+# Screen dimensions
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
