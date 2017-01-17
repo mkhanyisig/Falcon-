ac4e6b4dffc711626d708c065d5b265dad68a651
EricaLei98 <zhijun_0705@126.com>
2017-01-17 10:32:02 -0500

    clean version of every class

--- player.py
+++ player.py
@@ -1,45 +1,35 @@
 
-
+"""
+For managing the player.
+"""
 import pygame
 import level
-# import platforms 
+import obstacles
 from spritesheet_functions import SpriteSheet
 
  
 class Player(pygame.sprite.Sprite):
-    """ 
-        This class represents the bar at the bottom that the player
-        controls. 
-    """
-    state = None
- 
+
     # -- Methods
     def __init__(self):
-        """ Constructor function """
  
         # Call the parent's constructor
         super(Player,self).__init__()
  
         # -- Attributes
-        # Set speed vector of player
-        self.change_y = 5
+        # Set speed of flappying of the player
+        self.change_y = 0
  
-        # This holds all the images for the animated flappying of wings 
+        # Hold images for the animated wings
         self.flying_frames = []
-    
- 
-        # # What direction is the player facing?
-        # self.direction = "R"
- 
-        # List of sprites that will kill the bird
-        # (Should these sprites be the floor and ceiling?
-        # Then what about the moving obstacles.)
 
+        # change of level
         self.level = None
+
         #falcon flapping player sheet
         sprite_sheet = SpriteSheet("arts/graphics/falcon_spritesheet.png")
 
-        # Load all the right facing images into a list
+        # Load 
         image = sprite_sheet.get_image(0, 0, 130, 180)
         self.flying_frames.append(image)
         image = sprite_sheet.get_image(130, 0, 110, 180)
@@ -55,27 +45,6 @@ class Player(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
- 
-        # # Move left/right
-        # self.rect.x += self.change_x
-        # pos = self.rect.x + self.level.world_shift
-        # if self.direction == "R":
-        #     frame = (pos // 30) % len(self.walking_frames_r)
-        #     self.image = self.walking_frames_r[frame]
-        # else:
-        #     frame = (pos // 30) % len(self.walking_frames_l)
-        #     self.image = self.walking_frames_l[frame]
-
-        # # See if we hit anything
-        # block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
-        # for block in block_hit_list:
-        #     # If we are moving right,
-        #     # set our right side to the left side of the item we hit
-        #     if self.change_x > 0:
-        #         self.rect.right = block.rect.left
-        #     elif self.change_x < 0:
-        #         # Otherwise if we are moving left, do the opposite.
-        #         self.rect.left = block.rect.right
  
         # Move down
         self.rect.y += self.change_y
